0f9baaafa7cba60585424ea1180bf0ffe40ccfc2
Machine-Trainer <weijianzeng2020@gmail.com>
2023-05-25 18:18:15 -0500

    Initial Commit

---
+++ FetchCodingTest/FetchCodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingTestApp.swift
+//  FetchCodingTest
+//
+//  Created by Weijian Zeng on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
